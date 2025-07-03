d0c7320064733657ee6de7aaddf571df2f7f2679
Prince-gk <prince.georanja@gmail.com>
2025-07-03 08:12:34 +0300

    Refactor app.py for consistency and readability; update session handling and error responses. Clean up seed.py by removing unnecessary print statements.

--- server/seed.py
+++ server/seed.py
@@ -10,19 +10,18 @@ from models import db, Recipe, User
 fake = Faker()
 
 with app.app_context():
-    print("Deleting all records...")
+
     Recipe.query.delete()
     User.query.delete()
 
     fake = Faker()
-
-    print("Creating users...")
 
     # make sure users have unique usernames
     users = []
     usernames = []
 
     for i in range(20):
+
         username = fake.first_name()
         while username in usernames:
             username = fake.first_name()
@@ -40,7 +39,6 @@ with app.app_context():
 
     db.session.add_all(users)
 
-    print("Creating recipes...")
     recipes = []
     for i in range(100):
         instructions = fake.paragraph(nb_sentences=8)
@@ -58,4 +56,3 @@ with app.app_context():
     db.session.add_all(recipes)
 
     db.session.commit()
-    print("Complete.")
